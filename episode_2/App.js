6a9bfdbcb016679459e8b5afe69ef072608821ba
kriteshmoyal01 <kartikdewani01@gmail.com>
2025-04-04 12:33:51 +0530

    episode-2B

--- episode_2/App.js
+++ episode_2/App.js
@@ -1,19 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   [
-      React.createElement("div", { id: "child1" }, [
-          React.createElement("h1", null, "I'm an h1 tag"),
-          React.createElement("h2", null, "I'm an h2 tag"),
-      ]),
-      React.createElement("div", { id: "child2" }, [
-          React.createElement("h1", null, "I'm another h1 tag"),
-          React.createElement("h2", null, "I'm another h2 tag"),
-      ])
+    React.createElement("div", { id: "child1", key: "child1" }, [
+      React.createElement("h1", { key: "h1-child1" }, "I'm an h1 tag"),
+      React.createElement("h2", { key: "h2-child1" }, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2", key: "child2" }, [
+      React.createElement("h1", { key: "h1-child2" }, "I'm another h1 tag"),
+      React.createElement("h2", { key: "h2-child2" }, "I'm another h2 tag"),
+    ])
   ]
 );
-
-console.log(parent); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
