72f7a1ef928cb5eab829df6657029057b4cf7c16
kriteshmoyal01 <kartikdewani01@gmail.com>
2025-05-03 12:40:14 +0530

    episode_3

--- episode_3/App.js
+++ episode_3/App.js
@@ -1,20 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement("div", { id: "child1", key: "child1" }, [
-      React.createElement("h1", { key: "h1-child1" }, "I'm an h1 tag"),
-      React.createElement("h2", { key: "h2-child1" }, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2", key: "child2" }, [
-      React.createElement("h1", { key: "h1-child2" }, "I'm another h1 tag"),
-      React.createElement("h2", { key: "h2-child2" }, "I'm another h2 tag"),
-    ])
-  ]
+// Using React.createElement(we never use this again)
+// const heading = React.createElement("h1", { className: "heading" }, "Namaste React");
+
+// Using JSX syntax
+//const jsxheading = <h1  className="heading" tabIndex={1}>Namaste React using JSX 🚀</h1>;
+
+
+const elem = <span>React Element</span>;
+
+// JSX element
+const Title = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
 );
 
+const number = 10000;
+
+// Functional Component
+const HeadingComponent = () => {
+  console.log("Rendering HeadingComponent with number:", number);
+  return (
+    <div id="container">
+      {elem}
+      {Title}
+      <h1 className="heading">Namaste React Functional Component</h1>
+      <p>Number: {number}</p>
+    </div>
+  );
+};
+
+//console.log(HeadingComponent);
+
+
+
+// Create root and render JSX
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+//root.render(heading); // You can change to `heading` to test that too
+
+//this is how react compount render 
+root.render(<HeadingComponent />);
